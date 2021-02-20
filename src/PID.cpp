49e038157932f94b7bf9caf319d1ad3d18ce9634
Neha Verma <nehbehl@gmail.com>
2021-02-20 17:55:31 +0530

    Update PID.cpp
    Added code to implement Init, Update error and Total error methods

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,13 +12,21 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
-
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
+  d_error = cte-p_error;
+  p_error = cte;
+  i_error += cte;
 
 }
 
@@ -26,5 +34,6 @@ double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  return (-Kp * p_error) - (Ki * i_error) - (Kd * d_error);
+   // TODO: Add your total error calc here!
 }
